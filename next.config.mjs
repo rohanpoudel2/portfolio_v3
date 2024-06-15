e5e5ef36fa16c6eccb06c152a55836e7cf3a9731
Rohan Poudel <nsrapoudel@gmail.com>
2024-06-15 12:46:13 +0545

    Docker for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,14 @@ import remarkGfm from 'remark-gfm'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+    NEXT_PUBLIC_NODEMAILER_EMAIL: process.env.NEXT_PUBLIC_NODEMAILER_EMAIL,
+    NEXT_PUBLIC_NODEMAILER_PW: process.env.NEXT_PUBLIC_NODEMAILER_PW,
+    NEXT_PUBLIC_EMAIL: process.env.NEXT_PUBLIC_EMAIL,
+  },
 }
 
 const withMDX = nextMDX({
